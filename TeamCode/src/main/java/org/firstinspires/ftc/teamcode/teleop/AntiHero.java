0ecaa7bd723aea590677a0dedf0001f154ffc809
Lovetocode141 <Christian.heuser@circuitrunners.com>
2023-10-05 16:18:18 -0400

    more motors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/AntiHero.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/AntiHero.java
@@ -11,21 +11,40 @@ import com.qualcomm.robotcore.hardware.ServoImplEx;
 @TeleOp
 public class AntiHero extends CommandOpMode {
 
-    private double power = -1;
-    private DcMotorEx
+    private double liftPower = -1;
+    private double frontLeftPower;
+    private double backLeftPower;
+    private double frontRightPower;
+    private double backRightPower;
+    private DcMotorEx frontLeft, backLeft, frontRight, backRight;
+
     DcMotorEx leftLiftMotor, rightLiftMotor;
+    private double y = gamepad1.left_stick_y;
+    private double x = gamepad1.left_stick_x;
+    private double liftUp = gamepad2.left_trigger_liftUp;
+    private double liftDown = gamepad2.right_trigger_liftDown;
 
     @Override
     public void initialize() {
         leftLiftMotor = hardwareMap.get(DcMotorEx.class, "leftLift");
         rightLiftMotor = hardwareMap.get(DcMotorEx.class, "rightLift");
+        frontLeft = hardwarMap.get(DcMotorEx.class, "frontLeftWheel");
+        backLeft = hardwareMap.get(DcMotorEx.class, "backLeftWheel");
+        frontRight = harwareMap.get(DcMotorEx.class, "frontRightWheel");
+        backRight = hardwareMap.get(DcMotorEx.class, "backRightWheel");
+
     }
     @Override
     public void run() {
         super.run();
 
-        leftLiftMotor.setPower(power);
-        rightLiftMotor.setPower(-power);
+        leftLiftMotor.setPower(liftPower);
+        rightLiftMotor.setPower(-liftPower);
+        frontLeft.setPower(frontLeftPower);
+        backLeft.setPower(backLeftPower);
+        frontRight.setPower(frontRightPower);
+        backRight.setPower(backRightPower);
+
     }
 
 }
