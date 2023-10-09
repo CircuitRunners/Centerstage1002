c4ff8564fd747d68851eceb7242356d9f4113128
Lovetocode141 <Christian.heuser@circuitrunners.com>
2023-10-09 16:44:49 -0400

    variables

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/MainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/MainTeleOp.java
@@ -17,14 +17,25 @@ public class MainTeleOp extends CommandOpMode {
     private double backLeftPower;
     private double frontRightPower;
     private double backRightPower;
-    private double airplanePosition = 0.0;
+    private double initialAirplanePosition;
+    private double airplanePosition = initialAirplanePosition;
     private double leftClawPosition = 0.0;
     private double rightClawPosition = 0.0;
+    private double leftArmPosition = 0.0;
+    private double rightArmPosition = 0.0;
+    private double extendedArmPosition;
+    private double initialArmPosition;
+    private double initialClawPosition;
+    private double extendedClawPosition;
+    private double finalAirplanePosition;
+    private double intakePowerValue = 1;
+    private double outtakePowerValue = -1;
     private DcMotorEx frontLeft, backLeft, frontRight, backRight;
     DcMotorEx leftLiftMotor, rightLiftMotor;
     DcMotorEx intakeMotor;
     Servo airplaneServo;
-    Servo leftClawServo, rightClawServo;
+    Servo leftClawServo,
+    Servo leftArm, rightArm;
 
 
     @Override
@@ -39,6 +50,9 @@ public class MainTeleOp extends CommandOpMode {
         airplaneServo = hardwareMap.get(Servo.class, "Airplane");
         leftClawServo = hardwareMap.get(Servo.class, "Left Claw");
         rightClawServo = hardwareMap.get(Servo.class, "Right Claw");
+        leftArm = hardwareMap.get(Servo.class, "Left arm servo");
+        rightArm = hardwareMap.get(Servo.class, "Right arm servo");
+
 
     }
     @Override
@@ -51,12 +65,13 @@ public class MainTeleOp extends CommandOpMode {
         double rx = gamepad1.right_stick_x;
         double x2 = gamepad2.left_stick_x;
 
-        boolean intake = gamepad2.right_bumper;
-        boolean reverseIntake = gamepad2.left_bumper;
+        boolean intake = gamepad1.right_bumper;
+        boolean reverseIntake = gamepad1.left_bumper;
         boolean airPlaneLaunch = gamepad2.y;
         boolean leftClawToggle = gamepad2.x;
         boolean rightClawToggle = gamepad2.b;
         boolean bothClawToggle = gamepad2.a;
+        boolean armToggle = gamepad2.right_bumper;
 
 
         if (liftUp != 0) {
@@ -67,43 +82,53 @@ public class MainTeleOp extends CommandOpMode {
         }
 
         if (intake){
-            intakePower = 1;
+            intakePower = intakePowerValue;
         }
         else if (reverseIntake){
-            intakePower = -1;
+            intakePower = outtakePowerValue;
         }
         else{
             intakePower = 0;
         }
 
         if(airPlaneLaunch){
-            airplanePosition = 0.5;
+            airplanePosition = finalAirplanePosition;
         }
         if(leftClawToggle){
-            if(leftClawPosition == 0.0){
-                leftClawPosition = 0.25;
+            if(leftClawPosition == initialClawPosition){
+                leftClawPosition = extendedClawPosition;
             }
-            else if(leftClawPosition == 0.25){
-                leftClawPosition = 0.0;
+            else if(leftClawPosition == extendedClawPosition){
+                leftClawPosition = initialClawPosition;
             }
 
         }
         if(rightClawToggle){
-            if(rightClawPosition == 0.0){
-                rightClawPosition = 0.25;
+            if(rightClawPosition == initialClawPosition){
+                rightClawPosition = extendedClawPosition;
             }
-            else if(rightClawPosition == 0.25){
-                rightClawPosition = 0.0;
+            else if(rightClawPosition == extendedClawPosition){
+                rightClawPosition = initialClawPosition;
             }
         }
         if(bothClawToggle){
-            if(leftClawPosition == 0.0 && rightClawPosition == 0.0){
-                leftClawPosition = 0.25;
-                rightClawPosition = 0.25;
+            if(leftClawPosition == initialClawPosition && rightClawPosition == initialClawPosition){
+                leftClawPosition = extendedClawPosition;
+                rightClawPosition = extendedClawPosition;
             }
-            else if (leftClawPosition == 0.25 && rightClawPosition == 0.25){
-                leftClawPosition = 0.0;
-                rightClawPosition = 0.0;
+            else if (leftClawPosition == extendedClawPosition && rightClawPosition == extendedClawPosition){
+                leftClawPosition = initialClawPosition;
+                rightClawPosition = initialClawPosition;
+            }
+        }
+        if(armToggle){
+            if(leftArmPosition == initialArmPosition && rightArmPosition == initialArmPosition){
+                leftArmPosition = extendedArmPosition;
+                rightArmPosition = extendedArmPosition;
+            }
+            if(leftArmPosition == extendedArmPosition && rightArmPosition == extendedArmPosition){
+                leftArmPosition = initialArmPosition;
+                rightArmPosition = initialArmPosition;
             }
         }
         frontLeftPower = Range.clip(-(y-x), -1.0, 1.0);
