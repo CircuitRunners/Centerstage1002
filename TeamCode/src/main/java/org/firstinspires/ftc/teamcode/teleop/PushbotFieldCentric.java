f074557982a237f54f25d5a8a6a0e74fb05725c0
madelyndm <madelyn.mcgurk@circuitrunners.com>
2023-10-19 10:04:42 -0400

    field centric

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/PushbotFieldCentric.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/PushbotFieldCentric.java
@@ -10,20 +10,32 @@ import com.arcrobotics.ftclib.gamepad.GamepadKeys;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.util.Range;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 
 @TeleOp
-public class Pushbot extends CommandOpMode{
+public class PushbotFieldCentric extends CommandOpMode{
     private DcMotorEx frontLeft, backLeft, frontRight, backRight;
     private double frontLeftPower, backLeftPower, frontRightPower, backRightPower;
+    private BNO055IMU imu;
     @Override
     public void initialize(){
         frontLeft = hardwareMap.get(DcMotorEx.class, "frontLeftWheel");
         backLeft = hardwareMap.get(DcMotorEx.class, "backLeftWheel");
         frontRight = hardwareMap.get(DcMotorEx.class, "frontRightWheel");
         backRight = hardwareMap.get(DcMotorEx.class, "backRightWheel");
+        imu = hardwareMap.get(IMU.class, "imu");
+
+
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
+        imu.initialize(parameters);
     }
 
 
@@ -31,11 +43,23 @@ public class Pushbot extends CommandOpMode{
     public void run(){
         super.run();
         double y = -gamepad1.left_stick_y;
-        double x = gamepad1.right_stick_x;
-        frontLeftPower = Range.clip(-(y-x), -1.0, 1.0);
-        backLeftPower = Range.clip(-(y-x), -1.0, 1.0);
-        frontRightPower = Range.clip(y+x, -1.0, 1.0);
-        backRightPower = Range.clip(y+x, -1.0, 1.0);
+        double x = gamepad1.left_stick_x;
+        double rx = gamepad1.right_stick_x;
+
+        if(gamepad1.options){
+            imu.resetYaw();
+        }
+
+        double botHeading = imu.getRobotYawPitchRollAngle().getYaw(AngleUnit.RADIANS);
+        double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
+        double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
+        rotX = rotX * 1.1;
+
+        double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+        frontLeftPower = (rotY + rotX + rx) / denominator;
+        backLeftPower = (rotY - rotX + rx) / denominator;
+        frontRightPower = (rotY - rotX - rx) / denominator;
+        backRightPower = (rotY + rotX - rx) / denominator;
         frontLeft.setPower(frontLeftPower);
         backLeft.setPower(backLeftPower);
         frontRight.setPower(frontRightPower);
