32272e20c4aaa6a83d4b1dc26f7c0d901214af64
madelyndm <madelyn.mcgurk@circuitrunners.com>
2023-11-03 15:59:11 -0400

    ...

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/BlueAllianceAuto/BlueRightLinePixelPlacement.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/BlueAllianceAuto/BlueRightLinePixelPlacement.java
@@ -9,7 +9,7 @@ import com.acmerobotics.roadrunner.geometry.Vector2d;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 
-public class BlueCenterLinePixelPlacement {
+public class BlueRightLinePixelPlacement {
     private static double tile = 24;
     private static double half_tile = 12;
     private static double robot_len = 9;
@@ -19,12 +19,21 @@ public class BlueCenterLinePixelPlacement {
     private static Pose2d left_red = new Pose2d(-36, -61.5, Math.toRadians(90));
     private static Pose2d right_blue = new Pose2d(12, 61.5, Math.toRadians(-90));
     private static Pose2d left_blue = new Pose2d(-36, 61.5, Math.toRadians(-90));
-        public static void main(String[] args) {
 
-            drive.trajectorySequenceBuilder(right_blue)
-                                    drive.trajectorySequenceBuilder(right_blue)
-                                            .splineTo(new Vector2d(12, 33), Math.toRadians(-90))
-                                            .back(tile + square_edge)
-                                            .strafeLeft(tile * 2)
-                                            .build()
-                            );
+    public static void main(String[] args) {
+
+        SampleMecanumDrive drive = null;
+        drive.trajectorySequenceBuilder(right_blue);
+        TrajectorySequence Spline = drive.trajectorySequenceBuilder(right_blue)
+                .splineTo(new Vector2d(31, 30), Math.toRadians(180))
+                .build();
+        TrajectorySequence back = drive.trajectorySequenceBuilder(right_blue)
+                .back(5)
+                .build();
+        TrajectorySequence strafeRight = drive.trajectorySequenceBuilder(right_blue)
+                .strafeRight(tile + square_edge * 3)
+                .build();
+        TrajectorySequence backTile = drive.trajectorySequenceBuilder(right_blue)
+                .back(tile)
+                .build();
+    }}
