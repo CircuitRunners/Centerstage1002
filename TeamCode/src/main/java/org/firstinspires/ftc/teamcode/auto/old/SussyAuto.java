899b7b76c30dedea6624c996eed62d08534202c0
madelyndm <madelyn.mcgurk@circuitrunners.com>
2024-02-01 16:20:20 -0500

    Update SussyAuto.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/old/SussyAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/old/SussyAuto.java
@@ -28,7 +28,7 @@ import org.firstinspires.ftc.teamcode.utilities.PropLocation;
 import org.firstinspires.ftc.teamcode.utilities.Team;
 import org.firstinspires.ftc.teamcode.vision.TeamPropDetector;
 
-@Autonomous (name = "Bad Blue Audience")
+@Autonomous (name = "Sussy Blue Audience")
 public class SussyAuto extends CommandOpMode{
 
     private TrajectorySequence THREE_PIXEL_ON_BACKDROP;
@@ -36,10 +36,11 @@ public class SussyAuto extends CommandOpMode{
     private TrajectorySequence TO_STACK;
     private TrajectorySequence TO_BACKDROP;
     private TrajectorySequence TO_STACK_FROM_BACKDROP;
+    private TrajectorySequence RETREAT_FROM_BACKDROP;
 
 
     // Default Values
-    private PropLocation locationID = DEFAULT_PROP_LOCATION; // set to center by default
+    private PropLocation locationID = PropLocation.MIDDLE; // set to center by default
 
     private Pose2d startPose = Pose2dMapped(-39.75, 63, Math.toRadians(270.00));
 
@@ -89,19 +90,6 @@ public class SussyAuto extends CommandOpMode{
             case LEFT: {
                 PURPLE_GLOBAL = drive.trajectorySequenceBuilder(startPose)
                         .lineToLinearHeading(Pose2dMapped(-39.75, 33.91, Math.toRadians(270)))
-                        .lineTo(Vector2dMapped(-39.75, 36.66))
-                        .build();
-
-                TO_STACK = drive.trajectorySequenceBuilder(PURPLE_GLOBAL.end())
-                        .lineToLinearHeading(Pose2dMapped(-57, 36, Math.toRadians(360)))
-                        .build();
-
-                TO_BACKDROP = drive.trajectorySequenceBuilder(TO_STACK.end())
-                        .lineTo(Vector2dMapped(48.63, 36))
-
-                        .build();
-                TO_STACK_FROM_BACKDROP = drive.trajectorySequenceBuilder(TO_BACKDROP.end())
-                        .lineToLinearHeading(Pose2dMapped(-57, 36, Math.toRadians(360)))
                         .build();
             }
             case MIDDLE: {
@@ -115,45 +103,74 @@ public class SussyAuto extends CommandOpMode{
                         .build();
 
                 TO_BACKDROP = drive.trajectorySequenceBuilder(TO_STACK.end())
-                        .lineTo(Vector2dMapped(48.63, 36))
+                        .lineTo(Vector2dMapped(47, 36))
                         .build();
-                TO_STACK_FROM_BACKDROP = drive.trajectorySequenceBuilder(TO_BACKDROP.end())
+
+                RETREAT_FROM_BACKDROP = drive.trajectorySequenceBuilder(TO_BACKDROP.end())
+                        .lineTo(Vector2dMapped(40, 36))
+                        .build();
+
+                TO_STACK_FROM_BACKDROP = drive.trajectorySequenceBuilder(RETREAT_FROM_BACKDROP.end())
                         .lineToLinearHeading(Pose2dMapped(-57, 36, Math.toRadians(360)))
                         .build();
+
+
 
             }
             case RIGHT: {
                 PURPLE_GLOBAL = drive.trajectorySequenceBuilder(startPose)
                         .lineToLinearHeading(Pose2dMapped(-39.75, 33.91, Math.toRadians(270)))
-                        .lineTo(Vector2dMapped(-39.75, 36.66))
-                        .build();
-
-                TO_STACK = drive.trajectorySequenceBuilder(PURPLE_GLOBAL.end())
-                        .lineToLinearHeading(Pose2dMapped(-57, 36, Math.toRadians(360)))
-                        .build();
-
-                TO_BACKDROP = drive.trajectorySequenceBuilder(TO_STACK.end())
-                        .lineTo(Vector2dMapped(48.63, 36))
-
-                        .build();
-                TO_STACK_FROM_BACKDROP = drive.trajectorySequenceBuilder(TO_BACKDROP.end())
-                        .lineToLinearHeading(Pose2dMapped(-57, 36, Math.toRadians(360)))
                         .build();
             }
 
-//            TrajectorySequence STRAFE_PARK = drive.trajectorySequenceBuilder(TO_BACKDROP.end())
-//                    .lineTo(Vector2dMapped(48.63, 59.5))
-//                    .build();
+        TrajectorySequence STRAFE_PARK = drive.trajectorySequenceBuilder(RETREAT_FROM_BACKDROP.end())
+                    .lineTo(Vector2dMapped(47, 59.5))
+                    .build();
+
 
 
             schedule(
                     new SequentialCommandGroup(
                             new TrajectorySequenceCommand(drive, PURPLE_GLOBAL),
-                            new TrajectorySequenceCommand(drive, TO_STACK),
+                            new InstantCommand(claw::open),
+
+
+                            new ParallelCommandGroup(
+                                    new ParallelRaceGroup(
+                                            new IntakeStackCommand(hardwareMap, claw, intake, Intake.IntakePowers.FAST, extendo),
+                                            new WaitCommand(6000)
+                                    ),
+                                    new InstantCommand(extendo::mid),
+                                    new TrajectorySequenceCommand(drive, TO_STACK),
+                                    new SequentialCommandGroup(
+                                            new WaitCommand(500),
+                                            new InstantCommand(extendo::mid)
+                                    )
+
+                            ),
                             new TrajectorySequenceCommand(drive, TO_BACKDROP),
+
+                            new MoveToScoringCommand(lift, arm, claw, MoveToScoringCommand.Presets.MID),
+                            new InstantCommand(claw::open),
+                            new InstantCommand(()->lift.setLiftPower(-0.2)),
+                            new WaitCommand(300),
+                            new InstantCommand(()->lift.brake_power()),
+
+                            new TrajectorySequenceCommand(drive, RETREAT_FROM_BACKDROP),
+                            new RetractOuttakeCommand(lift, arm, claw),
+
                             new TrajectorySequenceCommand(drive, TO_STACK_FROM_BACKDROP),
-                            new TrajectorySequenceCommand(drive, TO_BACKDROP)
-                            //new TrajectorySequenceCommand(drive, STRAFE_PARK)
+                            new TrajectorySequenceCommand(drive, TO_BACKDROP),
+
+                            new MoveToScoringCommand(lift, arm, claw, MoveToScoringCommand.Presets.MID),
+                            new InstantCommand(claw::open),
+                            new InstantCommand(()->lift.setLiftPower(-0.2)),
+                            new WaitCommand(300),
+                            new InstantCommand(()->lift.brake_power()),
+
+                            new TrajectorySequenceCommand(drive, RETREAT_FROM_BACKDROP),
+                            new RetractOuttakeCommand(lift, arm, claw),
+                            new TrajectorySequenceCommand(drive, STRAFE_PARK)
 
                     )
 
